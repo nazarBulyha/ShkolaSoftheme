32fd5ed154d7c2273647902b80b4cc5a2c41ec9a
Nazar Bulyha <nazikkun97@gmail.com>
2017-11-30 18:20:54 +0200

    ConsoleWriteLine
    Deduces my name

---
+++ CSharpHW/1/HW1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Nazar Bulyha");
+
+            Console.ReadKey();
+        }
+    }
+}
